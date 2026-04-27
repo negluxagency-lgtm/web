460e6e62368bac655296890f3da14b4fbc62ee7a
Alex <neglux.agency@gmail.com>
2026-04-28 00:26:26 +0200

    ass

--- components/PwaRegistrar.tsx
+++ components/PwaRegistrar.tsx
@@ -7,13 +7,14 @@ const VAPID_PUBLIC_KEY = process.env.NEXT_PUBLIC_VAPID_PUBLIC_KEY!;
 function urlBase64ToUint8Array(base64String: string): Uint8Array {
   const padding = '='.repeat((4 - (base64String.length % 4)) % 4);
   const base64 = (base64String + padding).replace(/-/g, '+').replace(/_/g, '/');
-  const rawData = window.atob(base64);
+  const rawData = atob(base64);
   const outputArray = new Uint8Array(rawData.length);
-  for (let i = 0; i < rawData.length; ++i) {
+  for (let i = 0; i < rawData.length; i++) {
     outputArray[i] = rawData.charCodeAt(i);
   }
   return outputArray;
 }
+
 
 async function subscribeToPush(registration: ServiceWorkerRegistration) {
   try {
